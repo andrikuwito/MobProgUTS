a5e1b3cc6e3d0a29d5c46887911486e1b807f063
andrikuwito <andrikuwito@gmail.com>
2021-03-07 19:52:07 +0700

    commit on main

--- tradeapp/lib/main.dart
+++ tradeapp/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:UTS/homescreen.dart';
 import 'package:flutter/material.dart';
 import 'package:UTS/transaction.dart';
 import 'package:UTS/user.dart';
-import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
